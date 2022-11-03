bcd0a46e795a0a718fe7e932422877ac9b8ff5b5
gtengwei <gohtengwei1999@hotmail.com>
2022-11-04 01:43:44 +0800

    added developed rule

--- YongWenTengWei_qn_1_2.pl
+++ YongWenTengWei_qn_1_2.pl
@@ -4,11 +4,11 @@ company(appy) .
 competitor(sumsum,appy) .
 smartphonetechnology(galactica-s3) .
 boss(stevey, appy) .
-developed(sumsum, galactica-s3) .
 stolen(stevey, galactica-s3) .
 
 /* rules */
+developed(X,Y) :- company(X), smartphonetechnology(Y) .
 business(X) :- smartphonetechnology(X) .
 competitor(X, Y) :- competitor(Y, X) .
 rival(X, Y) :- competitor(X, Y) .
-unethical(X) :- boss(X, Y), stolen(X, Z), developed(W, Z), business(Z),company(W), rival(Y, W) .
+unethical(X) :- boss(X, Y), stolen(X, Z), developed(W, Z), rival(Y, W) .
