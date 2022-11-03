09f7142bcd8ebb39ee617ed1dcc8554591753f15
gtengwei <gohtengwei1999@hotmail.com>
2022-11-04 02:04:29 +0800

    changed developed rule to techDevelopedBy

--- YongWenTengWei_qn_1_2.pl
+++ YongWenTengWei_qn_1_2.pl
@@ -2,13 +2,14 @@
 company(sumsum) .
 company(appy) .
 competitor(sumsum,appy) .
-smartphonetechnology(galactica-s3) .
+smartphoneTechnology(galactica-s3) .
+developed(sumsum,galactica-s3) .
 boss(stevey, appy) .
 stolen(stevey, galactica-s3) .
 
 /* rules */
-developed(X,Y) :- company(X), smartphonetechnology(Y) .
-business(X) :- smartphonetechnology(X) .
+techDevelopedBy(X,Y) :- business(X), company(Y) .
+business(X) :- smartphoneTechnology(X) .
 competitor(X, Y) :- competitor(Y, X) .
 rival(X, Y) :- competitor(X, Y) .
-unethical(X) :- boss(X, Y), stolen(X, Z), developed(W, Z), rival(Y, W) .
+unethical(X) :- boss(X, Y), stolen(X, Z), developed(W,Z), techDevelopedBy(Z,W), rival(Y, W) .
