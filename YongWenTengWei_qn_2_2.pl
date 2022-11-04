0a55b77c4d67e8800bbd7ab012cb02e97c8af054
gtengwei <gohtengwei1999@hotmail.com>
2022-11-04 18:46:46 +0800

    changed rules and predicates for qn2_2

--- YongWenTengWei_qn_2_2.pl
+++ YongWenTengWei_qn_2_2.pl
@@ -1,12 +1,41 @@
-child(charles) .
-child(ann) .
-child(andrew) .
-child(edward) . 
+/* predicates */
+male(prince_charles) .
+male(prince_andrew) . 
+male(prince_edward) .
 
-parent(elizabeth, charles) .
-parent(elizabeth, ann) .
-parent(elizabeth, andrew) .
-parent(elizabeth, edward) .
+female(queen_elizabeth) .
+female(princess_ann) .
+
+parent(queen_elizabeth, prince_charles) .
+parent(queen_elizabeth, prince_andrew) .
+parent(queen_elizabeth, prince_edward) .
+parent(queen_elizabeth, princess_ann) .
 
 
-successor(X, Y) :- parent(X, Y) .
+older(prince_charles, princess_ann) .
+older(princess_ann, prince_andrew) .
+older(prince_andrew, prince_edward) .
+
+/* rules */
+
+is_older(X, Y) :- older(X, Y) .
+is_older(X, Y) :- older(X, Z), is_older(Z, Y) .
+
+son(X, Y) :- parent(Y,X), male(X) .
+daughter(X, Y) :- parent(Y,X), female(X) .
+
+succession_rule(X,Y) :- parent(Z, X), parent(Z, Y), 
+                        is_older(X, Y),
+                        Y\=queen_elizabeth .
+                          
+
+%% Sorting algorithm
+succession_sort([A|B], Sorted) :- succession_sort(B, SortedTail), insert(A, SortedTail, Sorted).
+succession_sort([], []).
+
+insert(A, [B|C], [B|D]) :- not(succession_rule(A,B)), !, insert(A, C, D).
+insert(A, C, [A|C]).
+
+royalSuccessionList(X, SuccessionList):-
+	findall(Y, parent(X,Y), Children),
+	succession_sort(Children, SuccessionList).
