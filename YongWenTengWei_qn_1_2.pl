28f0ddb6f35514bd944c7f5ef4a9800b66b1cbb4
gtengwei <gohtengwei1999@hotmail.com>
2022-11-04 22:47:08 +0800

    edited predicate name

--- YongWenTengWei_qn_1_2.pl
+++ YongWenTengWei_qn_1_2.pl
@@ -2,14 +2,14 @@
 company(sumsum) .
 company(appy) .
 competitor(sumsum,appy) .
-smartphoneTechnology(galactica-s3) .
+smart_phone_technology(galactica-s3) .
 developed(sumsum,galactica-s3) .
 boss(stevey, appy) .
 stolen(stevey, galactica-s3) .
 
 /* rules */
-techDevelopedBy(X,Y) :- developed(Y,X), business(X), company(Y) .
-business(X) :- smartphoneTechnology(X) .
+tech_developed_by(X,Y) :- developed(Y,X), business(X), company(Y) .
+business(X) :- smart_phone_technology(X) .
 competitor(X, Y) :- competitor(Y, X) .
 rival(X, Y) :- competitor(X, Y) .
-unethical(X) :- boss(X, Y), stolen(X, Z), techDevelopedBy(Z,W), rival(Y, W) .
+unethical(X) :- boss(X, Y), stolen(X, Z), tech_developed_by(Z,W), rival(Y, W) .
